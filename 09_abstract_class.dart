a5cbc2301f9cd4b339e0060b3208646974997209
Alexis <floresfernandezalexis05@gmail.com>
2024-11-03 14:11:01 -0600

    use implements

--- 09_abstract_class.dart
+++ 09_abstract_class.dart
@@ -1,8 +1,11 @@
 void main() {
   
   final windPlant = WindPlant( initialEnergy: 100);
+  final nuclearPlant = NuclearPlant(energyLeft: 1000);
+
   
   print(' wind: ${ chargePhone( windPlant ) }');
+  print(' nuclear: ${ chargePhone( nuclearPlant ) }');
   
   
 }
@@ -23,7 +26,7 @@ enum PlantType { nuclear, wind, water }
 abstract class EnergyPlant {
   
   double energyLeft;
-  PlantType type; // nuclear, wind, water
+  final PlantType type; // nuclear, wind, water
   
   
   EnergyPlant({ 
@@ -44,6 +47,23 @@ class WindPlant extends EnergyPlant {
   @override
   void consumeEnergy( double amount  ) {
     energyLeft -= amount;
+  }
+  
+}
+
+class NuclearPlant implements EnergyPlant {
+  
+  @override
+  double energyLeft;
+  
+  @override
+  final PlantType type = PlantType.nuclear;
+  
+  NuclearPlant({ required this.energyLeft });
+  
+  @override
+  void consumeEnergy( double amount) {
+    energyLeft -= (amount + 0.5);
   }
   
 }
