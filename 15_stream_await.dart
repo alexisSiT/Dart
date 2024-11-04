55e048780eb994a0c65970cf4bc07475eb636c56
Alexis <floresfernandezalexis05@gmail.com>
2024-11-04 07:28:12 -0600

    stream await

---
+++ 15_stream_await.dart
@@ -0,0 +1,23 @@
+void main() {
+  
+  emitNumbers()
+    .listen( (int value) {
+      
+      print('Stram value: $value');
+      
+    });
+  
+  
+}
+
+
+Stream<int> emitNumbers() async* {
+  
+  final valuesToEmit = [1,2,3,4,5];
+  
+  for(int i in valuesToEmit) {
+    await Future.delayed( const Duration(seconds: 1 ));
+    yield i;
+  }
+  
+}
